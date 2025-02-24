acd52c406b98c7795be41a01ab6806d87c2943bd
imprathamraj <rjpratham05@gmail.com>
2025-02-24 13:49:43 +0530

    [Pratham Raj] Add. AddressBookRepository file and application.properties file

---
+++ src/main/java/com/bridgelabz/addressbookapplication/repository/AddressBookRepository.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbookapplication.repository;
+
+import com.bridgelabz.addressbookapplication.model.AddressBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressBookRepository extends JpaRepository<AddressBook,Long> {
+}
